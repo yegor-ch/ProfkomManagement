0598f9372afe7f40a7725bb8fce3f68677705274
egor.chasnovskii <egor.chasnovskii@gmail.com>
2020-02-02 16:18:55 +0200

    Added RegisterViewModel

---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProfkomManagement.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Паролі мають співадати.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
